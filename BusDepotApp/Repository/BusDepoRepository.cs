1d8d4a58859e1efc46c26fa1c2c6a3e3a3dd26b6
nitikamehta <40000940+nitikamehta@users.noreply.github.com>
2020-08-04 13:14:05 +0530

    Adding code for the Repo,Service, Controller Testing the Api connection on postman

--- BusDepotApp/Repository/BusDepoRepository.cs
+++ BusDepotApp/Repository/BusDepoRepository.cs
@@ -8,15 +8,26 @@ namespace BusDepotApp.Repository
 {
     public class BusDepoRepository : IBusDepoRepository
     {
-        private readonly List<Buses> busList = new List<Buses>() {
+        private  List<Buses> busList = new List<Buses>() {
             new Buses(){ BusNumber="DL03PM2320",Id=1,Capacity=50, Occupency=25, CurrentFuelConsumed=60,
             FuelTankCapacity=200 },
-            new Buses(){ BusNumber="DL02PM4520",Id=1,Capacity=50, Occupency=25, CurrentFuelConsumed=60,
+            new Buses(){ BusNumber="DL02PM4520",Id=2,Capacity=50, Occupency=25, CurrentFuelConsumed=60,
             FuelTankCapacity=200 },
-            new Buses(){ BusNumber="DL02PM5620",Id=1,Capacity=50, Occupency=25, CurrentFuelConsumed=60,
+            new Buses(){ BusNumber="DL02PM5620",Id=3,Capacity=50, Occupency=25, CurrentFuelConsumed=60,
             FuelTankCapacity=200 }
         };
+        public BusDepoRepository()
+        {
+            busList[0].CurrentLocation = new Location() { Id = 1, Latitude = 22.2, Longitude = 43.2 };
+            busList[1].CurrentLocation = new Location() { Id = 2, Latitude = 27.2, Longitude = 48.2 };
+            busList[2].CurrentLocation = new Location() { Id = 3, Latitude = 264, Longitude = 234 };
+        }
+       
 
+        public IEnumerable<Maintenance > GetMaintenanceInfo( int busId)
+        {
+            return busList.FirstOrDefault(b => b.Id == busId).Maintenances;
+        }
         public IEnumerable<Buses> GetAllBuses()
         {
             return busList.ToList();
@@ -28,7 +39,11 @@ namespace BusDepotApp.Repository
             return bus;
         }
         
-
+        public Location GetBusLocation(int busId)
+        {
+            var busLocation = busList.FirstOrDefault(b => b.Id == busId).CurrentLocation;
+            return busLocation;
+        }
            
     }
 }
--- BusDepotApp/Services/IBusDepoService.cs
+++ BusDepotApp/Services/IBusDepoService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusDepotApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,9 @@ namespace BusDepotApp.Services
 {
    public interface IBusDepoService
     {
+        Buses GetBus(int busid);
+        IEnumerable<Buses> GetAllBus();
+        Location GetBusLocation(int busId);
 
     }
 }
