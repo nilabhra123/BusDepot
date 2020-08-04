795dd1c13e87bd5f777c47f4226f9dfc410ad1ad
NILABHRA <nilabhra123@gmail.com>
2020-08-04 11:07:34 +0530

    Models Update

---
+++ BusDepotApp/Models/Maintenance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusDepotApp.Models
+{
+    public class Maintenance
+    {
+        public int Id { get; set; }
+        public string MaintenceTitle { get; set; }
+        public int MaintenanceOccuarance { get; set; }
+        public int EligibleDistance { get; set; }
+        public int EligibleDurationInMonths { get; set; }
+        public DateTime MaintenanceDate { get; set; }
+    }
+}
