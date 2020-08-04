3ef9936495663e339c03190e3dd73586d316ad68
vishal8860 <vishal.pandey8860@gmail.com>
2020-08-04 12:08:52 +0530

    created map component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import SimpleMap from './components/Map/Map'
  
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
-  };
- 
+class App extends Component {
   render() {
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '50vh', width: '50%' }}>
-        <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyAI00YsVSS4dzveaqPsvWKj92EQbNOTtJU" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My bus"
-          />
-        </GoogleMapReact>
-      </div>
+      </SimpleMap>
     );
   }
 }
  
-export default SimpleMap;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import SimpleMap from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <SimpleMap />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
